17fea25e5f01674b72252fe4b52dd83e31b62574
TonyHolby <tonynemoutheholberton@gmail.com>
2024-06-25 20:41:25 +0200

    Implémentation de la classe Datamanger dans manage.py pour gérer la persistance des données

--- manage.py
+++ manage.py
@@ -1,9 +1,30 @@
 """ Entry point for the application. """
 
+from src import db, create_app
+from src.persistence import FileRepository
 from flask.cli import FlaskGroup
-from src import create_app
 
+
+app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
+
+class DataManager:
+    def save_user(self, user):
+        if app.config['USE_DATABASE']:
+            db.session.add(user)
+            db.session.commit()
+        else:
+            file_repository = FileRepository()
+            file_repository._save_to_file(data=user)
+
+
+@app.cli.command("init_db")
+def init_db():
+    """ Initialize the database. """
+    with app.app_context():
+        db.create_all()
+        print("Database initialized.")
 
 
 if __name__ == "__main__":
